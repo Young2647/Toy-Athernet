a5f4b7a89b7301e36b0559b5100fcee4f0e74f27
zhaoyboo <zhaoyb1@shanghaitech.edu.cn>
2021-11-18 19:06:52 +0800

    debugging

--- MAClayer/Source/receiver.cpp
+++ MAClayer/Source/receiver.cpp
@@ -244,6 +244,7 @@ Receiver::Demodulate(float sample)
 Array<int8_t>
 Receiver::getData()
 {
+    const ScopedLock sl(lock);
     data_state = SYNC;
     std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
     while (data_state != DATA_RECEIVED)
