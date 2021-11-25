ed35a964a29eae1d44ef06eae78320d6be36929f
yanghd <yanghd@shanghaitech.edu.cn>
2021-11-25 13:55:29 +0800

    newest version for part2

--- MAClayer/Source/defines.h
+++ MAClayer/Source/defines.h
@@ -20,15 +20,15 @@
 
 #define DEFAULT_RWS 4
 #define MAX_WAITING_TIME 1500
-#define MAX_RESEND_TIME 5
+#define MAX_RESEND_TIME 10
 
 #define FRAME_OFFSET 32
 #define CRC_LEN 8
 
-#define MAX_BYTE_NUM 6250
+#define MAX_BYTE_NUM 5000
 #define DEAFULT_WINDOW_SIZE 63
 
-#define DEFAULT_RECEIVE_NUM 5000
+#define DEFAULT_RECEIVE_NUM 50
 // address
 #define YHD 0x01
 #define ZYB 0x10
