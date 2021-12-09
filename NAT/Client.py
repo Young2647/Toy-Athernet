9929b07c758343686837ba9992a8739ae90374e3
yanghd <yanghd@shanghaitech.edu.cn>
2021-12-09 21:04:45 +0800

    part1 client finished

--- NAT/Client.py
+++ NAT/Client.py
@@ -2,9 +2,14 @@ import socket
 import sys
 import os
 import time
+import msvcrt
+import random
+import string
 
 def randomString(n):
-    return os.urandom(n)
+    payload = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20 )) 
+    return bytes(payload, encoding='utf8')
+
 
 class Client :
     def __init__(self, ip, port) :
@@ -15,14 +20,14 @@ class Client :
         self.clientsocket.sendto(data, self.ip_port)
 
     def StopClient(self) :
-        self.sendData("Exit")
+        self.sendData("Exit".encode())
         print("Client has stopped.")
         self.clientsocket.close()
 
 if __name__ == "__main__" :
-    client = Client("10.19.73.48", 23333)
+    client = Client("10.20.225.5", 23333)
     while True :
-        if input() :
+        if msvcrt.kbhit() :
             break
         t = time.time()
         data = randomString(20)
