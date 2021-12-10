4788133bb85c65d7777e2285838aa713d16db5a1
yanghd <yanghd@shanghaitech.edu.cn>
2021-12-10 13:11:53 +0800

    update main for node2 receiving process

--- MAClayer/Source/Main.cpp
+++ MAClayer/Source/Main.cpp
@@ -9,6 +9,7 @@
 #include <JuceHeader.h>
 #include <fstream>
 #include <conio.h>
+#include <stdlib.h>
 #include "sender.h"
 #include "receiver.h"
 #include "defines.h"
@@ -16,7 +17,7 @@
 #include "MAClayer.h"
 using namespace juce;
 using namespace std;
-
+#define MODE_UDP_NODE2 0x0a
 
 
 
@@ -32,6 +33,41 @@ int main(int argc, char* argv[])
     dev_info = dev_manager.getAudioDeviceSetup();
     dev_info.sampleRate = 48000; // Setup sample rate to 48000 Hz
     dev_manager.setAudioDeviceSetup(dev_info, false);
+    int mode = MODE_UDP_NODE2;
+    if (mode == MODE_UDP_NODE2)
+    {
+        std::fstream notify_file;
+        notify_file.open("WRITE_DOWN.txt", ios::in);
+        cout << "Waiting for python to notify...\n";
+        while (!notify_file)
+        {
+            notify_file.open("WRITE_DOWN.txt", ios::in);
+        }
+        if (notify_file) remove("WRITE_DOWN.txt");
+        int num_bits_per_frame = 408; // 51 bytes
+        int num_frame = 30; //30 frames
+        std::unique_ptr<MAClayer> mac_layer;
+        if (mac_layer.get() == nullptr)
+        {
+            mac_layer.reset(new MAClayer(3, num_bits_per_frame, num_frame, YHD, ZYB, 20));
+        }
+        dev_manager.addAudioCallback(mac_layer.get());
+        mac_layer.get()->StartMAClayer();
+        auto start_time = std::chrono::system_clock::now();
+        std::cout << "Press any ENTER to stop MAClayer.\n";
+        while (!mac_layer.get()->getStop())
+        {
+            if (kbhit()) mac_layer.get()->callStop(1);
+        }
+        mac_layer.get()->StopMAClayer();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count();
+        std::cout << "Transmit time : " << duration << "ms.\n";
+        dev_manager.removeAudioCallback(mac_layer.get());
+        DeletedAtShutdown::deleteAll();
+        juce::MessageManager::deleteInstance();
+
+        return 0;
+    }
 
     int num_bits_per_frame = 840;
 
