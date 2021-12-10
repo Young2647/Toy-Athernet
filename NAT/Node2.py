c5976dbff11db3854b7fbd12c0dad75f6f2e0678
yanghd <yanghd@shanghaitech.edu.cn>
2021-12-10 23:28:07 +0800

    update node2 for icmp req TODO: checksum

--- NAT/Node2.py
+++ NAT/Node2.py
@@ -1,11 +1,17 @@
 import socket
+from typing_extensions import final
 from Client import Client
 from Client import Getch
 from Server import Server
 import os
 import msvcrt
 import time
+import struct
+import random
+import select
 
+ICMP_ECHO_REQUEST = 8
+DEFALT_TIMEOUT = 1
 class Node2 :
     def __init__(self, debug_on = False, ip = "10.20.225.5", port = 23333, isClient = False):
         if isClient :
@@ -59,25 +65,95 @@ class Node2 :
         inputfile.write(address[1].to_bytes(2, byteorder = 'little'))
         inputfile.write(data)
     
+    def checkNotify(self) :
+        if os.path.exists("WRITE_DOWN.txt") :
+            os.remove("WRITE_DOWN.txt")
+
     def notifyAther(self) :
         f = open ("WRITE_DOWN.txt","w")
         if (self.debug_on) :
             print("Notify the AtherNode to work.")
         f.close()
+        time.sleep(0.04)
+        os.remove("WRITE_DOWN.txt")
 
+    def ICMPecho(self) :
+        while True :
+            if Getch() : break
+            if os.path.exists("ICMP_NOTIFY.txt") :
+                os.remove("ICMP_NOTIFY.txt")
+                break
+        if os.path.exists("OUTPUT.bin") :
+            with open("OUTPUT.bin","rb") as f :
+                ping_address = f.read(4)
+                data = f.read()
+            ping_address = socket.inet_ntoa(ping_address) #bytes to address
+            if (self.debug_on) :
+                print("request ping from :", ping_address)    
+            packet = self.generateICMPpacket(ping_address,data)
+            self.ping_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) #raw socket
+            self.ping_socket.sendto(packet, (ping_address, 2333))
+            self.req_time = time.time()
+            reply_time = self.getReplypacket(data)
+            if reply_time == -1 :
+                print("ICMP ECHO failed!")
+            else :
+                print("ICMP reply in ", reply_time, " s")
+                with open("reply.txt", 'wb') as f:
+                    f.write(ping_address)
+                    f.write(data)
+                self.notifyAther() #notify atherNode to work
+
+    def getReplypacket(self,data) :
+        time_remain = DEFALT_TIMEOUT
+        final_time = -1
+        while True :
+            start_time = time.time()
+            result = select.select([self.ping_socket], [], [], time_remain)
+            if result[0] == [] : break
+            end_time = time.time()
+            reply_packet, address = self.ping_socket.recvfrom(1024)
+            header = reply_packet[20:28]
+            icmp_type, code, check_sum, id, sequence = struct.unpack('bbHHh', reply_packet)
+            dummy_header = struct.pack('bbHHh', icmp_type, code, 0, id, sequence)
+            if id == self.ping_id and check_sum == self.calculateChecksum(dummy_header + data) :
+                final_time = end_time - self.req_time
+                break
+            else :
+                time_remain -= end_time - start_time
+            if time_remain <= 0 : break
+        return final_time
+    def calculateChecksum(self, data) :
+        s = 1 #waiting to be implemented
+
+    def generateICMPpacket(self,ping_address,data) :
+        self.ping_id = random(range(0,1000))
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, 0, self.ping_id, 1) #dummy header without checksum
+        check_sum = self.calculateChecksum(header + data)
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, check_sum, id, 1) # real header
+        return header + data
 SEND = 0
 RECEIVE = 1
+ICMP = 2
 
 if __name__ == "__main__" :
-    if input("If send to node3, press s. If receive from node3, press r\n") == "r" :
+    key = input("If send to node3, press s. If receive from node3, press r. If ICMP, press p\n")
+    if key == "r" :
         mode = RECEIVE
-    else :
+    elif key == "s":
         mode = SEND
+    elif key == "p":
+        mode = ICMP
     if mode == SEND :
         node2 = Node2(True,"10.20.220.107", isClient=True)
         node2.receiveFromNode1()
         node2.sendToNode3()
     elif mode == RECEIVE :
         node2 = Node2(True,"10.20.220.107", isClient=False)
+        node2.checkNotify()
         node2.receiveFromNode3()
         node2.notifyAther()
+    elif mode == ICMP :
+        node2 = Node2(True, isClient=True)
+        node2.checkNotify()
+        node2.ICMPecho()
